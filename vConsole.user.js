b481ac55814fe798d917e83dd3a214f6dfe9bfc5
xiangjianwen <38367463+xiangjianwen@users.noreply.github.com>
2018-10-05 22:14:20 +0800

    Add files via upload

---
+++ vConsole.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name vConsole
+// @namespace Violentmonkey Scripts
+ // @require https://cdnjs.cloudflare.com/ajax/libs/vConsole/3.2.0/vconsole.min.js
+// @match *://*/*
+// @grant none
+// ==/UserScript==
+ var vConsole = new VConsole();  
+console.log('Hello world');
